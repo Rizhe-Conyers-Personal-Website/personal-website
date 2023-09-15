e5db89c15f249b6a3a011177e03e7478b1097811
rizthewiz <rizthewiz>
2023-09-14 20:53:10 -0400

    added, images, footer, javascript and css to style webpages as well as some content. Rough draft ready

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const notice =
+  "This website is still underconstruction and will be updated throughout the course, bare with me please.";
+
+function underconstruction() {
+  alert(notice);
+}
+
+underconstruction();
